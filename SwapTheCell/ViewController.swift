6fc2fdc1308a69ce94e696ea533a11edc21eb95a
Vladimir Oleinikov <dragon2020@inbox.ru>
2022-04-15 09:46:41 +0300

    Initial Commit

---
+++ SwapTheCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwapTheCell
+//
+//  Created by Владимир Олейников on 15/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
